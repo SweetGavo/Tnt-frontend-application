55a972ff59db88bce9f3b820220d4ba7e1eac8cb
glory <aremuopomulero@gmail.com>
2022-10-12 12:04:44 +0100

    messages

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AuthLayout from "../../layouts/authLayout";
 import TextField from "../../components/textField";
 import Button from "../../components/button";
 import image from "../../public/images/registration.jpg";
 import Image from "next/image";
 import style from "../../styles/Registration.module.scss";
+import {url} from "../../utils/urlHelpers";
+const defaultValue = {
+    firstName:"",
+    lastName:"",
+    email:"",
+    password:""
+}
 
 function Index(props) {
+    const [form,setForm] = useState(defaultValue);
+    const [response,setResponse] = useState({
+        type:'error',
+        message:""
+    })
+    function setValue({target}){
+        const {name,value} = target;
+            setForm(v=>({...v, [name]:value}));
+    }
+
+    function submitForm(){
+                get( url.loginUrl,form)
+                    .then(resp=>{
+
+                    })
+                    .catch((e)=>{
+
+                    }).finally(e=>{
+
+                });
+    }
     return (
         <div className={'flex registration-container'}>
 
@@ -21,13 +49,17 @@ function Index(props) {
                 <div className={style.formContainer}>
                     <h1>Create an account</h1>
                     <p>Kindly enter your details below to create an account</p>
-                    <TextField label={'Full name'} placeholder={'Jonathan Doe'}/>
-                    <TextField label={'Email Address'} type={'email'} placeholder={'E.g jonathandoe@gmail.com'}/>
-                    <TextField label={'Password'} type={'password'} placeholder={'password'} />
+                    {
+                        
+                    }
+                    <TextField value={form.firstName} onChange={setValue} label={'First name'} placeholder={'Jonathan'}/>
+                    <TextField value={form.lastName} onChange={setValue} label={'Last name'} placeholder={'Doe'}/>
+                    <TextField  value={form.email} onChange={setValue} label={'Email Address'} type={'email'} placeholder={'E.g jonathandoe@gmail.com'}/>
+                    <TextField  value={form.password} onChange={setValue} label={'Password'} type={'password'} placeholder={'password'} />
                     <Button size={'sm'} style={'blue'} radius={5} className={style.buttons} >
                         Create account
                     </Button>
-                    <Button size={'sm'} variant={'outline'} className={style.buttons} radius={5} >
+                    <Button size={'sm'} variant={'outline'} onClick={submitForm} className={style.buttons} radius={5} >
                         <img src="/images/glogo.png" className={style.gLogo} alt=""/> Continue with Google
                     </Button>
                 </div>
