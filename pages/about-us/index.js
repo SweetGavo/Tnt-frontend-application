a94995123fd1b12b6f9e11f227eea9cd6a1c307e
Gavin <longgaving@gmail.com>
2022-10-28 00:56:26 +0100

    update aboutpage

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -11,39 +11,24 @@ function Index(props) {
       <div className={style.main}>
         <section>
           <div>
-            <span>About us</span>
+            <h1>About page</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
-              amet commodo nulla facilisi. Posuere urna nec tincidunt praesent
-              semper feugiat. Sit amet cursus sit amet dictum sit. Et malesuada
-              fames ac turpis egestas integer eget. Vulputate dignissim
-              suspendisse in est suspendisse.
+              T & T deals market limited is a subsidiary of Miraton Matador
+              group, registered and incorporated with CAC in May 1992, with a
+              proven success in general trading , acquisition and re-sale of
+              goods and services for individuals and organizations.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
-              amet commodo nulla facilisi. Posuere urna nec tincidunt praesent
-              semper feugiat. Sit amet cursus sit amet dictum sit. Et malesuada
-              fames ac turpis egestas integer eget. Vulputate dignissim
-              suspendisse in est ante in nibh mauris. Ac tortor vitae purus
-              faucibus ornare suspendisse.
+              Our years of experience, rich database and technical expertise in
+              the market has place us well above our contemporaries. Our team
+              consist of consultants and experts.
             </p>
 
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
-              amet commodo nulla facilisi. Posuere urna nec tincidunt praesent
-              semper feugiat. Sit amet cursus sit amet dictum sit. Et malesuada
-              fames ac turpis egestas integer eget. Vulputate dignissim
-              suspendisse in est ante in nibh mauris. Ac tortor vitae purus
-              faucibus ornare suspendisse.
+              In 2020, after the lockdown, we were able to grow our customer
+              almost 70%, where we on-board Integrated Corporate Services,
+              selling well over 40 vehicle in the midst of the covid-19 economy
+              crisis.
             </p>
             <div>
               <img
@@ -62,38 +47,20 @@ function Index(props) {
           </div>
           <div>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
-              amet commodo nulla facilisi. Posuere urna nec tincidunt praesent
-              semper feugiat. Sit amet cursus sit amet dictum sit. Et malesuada
-              fames ac turpis egestas integer eget. Vulputate dignissim
-              suspendisse in est ante in nibh mauris. Ac tortor vitae purus
-              faucibus ornare suspendisse.
+              We are constantly growing our customer base as well developing new
+              market strategies to enable us reach various client in different
+              sphere of the corporate world. Since incorporated, we have done
+              business with organization like ; Rosabon financial services,
+              Aquila leasing Company and also the telecommunication giant
+              Globacom Nigeria.
             </p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
-              amet commodo nulla facilisi. Posuere urna nec tincidunt praesent
-              semper Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
+              For the past years, we have generated over #6 billion naira in
+              revenue across all business , we are intentional about fulfilment
+              of our obligations to our customers which is to drive revenue and
+              profits for both parties.
             </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi in
-              nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              Varius sit amet mattis vulputate enim nulla. Sollicitudin nibh sit
-              amet commodo nulla facilisi. Posuere urna nec tincidunt praesent
-              semper feugiat. Sit amet cursus sit amet dictum sit. Et malesuada
-              fames ac turpis egestas integer eget. Vulputate dignissim
-              suspendisse in est ante in nibh mauris. Ac tortor vitae purus
-              faucibus ornare suspendisse.
-            </p>
+
             <div className={style.btn}>
               <Button
                 size={"sm"}
@@ -132,7 +99,22 @@ function Index(props) {
               <td className={style.bor}></td>
             </tr>
             <tr>
-              <td className={style.bor}>4</td>
+              <td className={style.bor}>5</td>
+              <td className={style.bor}></td>
+              <td className={style.bor}></td>
+            </tr>
+            <tr>
+              <td className={style.bor}>6</td>
+              <td className={style.bor}></td>
+              <td className={style.bor}></td>
+            </tr>
+            <tr>
+              <td className={style.bor}>7</td>
+              <td className={style.bor}></td>
+              <td className={style.bor}></td>
+            </tr>
+            <tr>
+              <td className={style.bor}>8</td>
               <td className={style.bor}></td>
               <td className={style.bor}></td>
             </tr>
@@ -141,32 +123,70 @@ function Index(props) {
       </section>
       <section>
         <div className={style.greenback}>
-          <p>What we do with ease</p>
+          <div>
+            <h1 style={{ color: "white" }} className={style.bold}>
+              What we do with ease
+              <p style={{ color: "white" }}>
+                Over the years , we have built an unbeatable trust between our
+                clients and partners
+              </p>
+            </h1>
+          </div>
           <div className={style.whitesectionBack}>
-            <div>
+            <div className={style.innerSecDivs}>
+              <img src="background: url(trend.png)" />
+              <h3>Free valuation</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi
-                in nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                Unused asset and asset with zero shelf life can be uploaded to
+                increase asset turn around time in corporations.
               </p>
             </div>
-            <div>
+
+            <div className={style.innerSecDivs}>
+              <img src="background: url(trend.png)" />
+
+              <h3>Instant decision</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi
-                in nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                Create up-to-date designs without writing a single line of code.
+                Build clean, minimalistic, beautifully crafted websites
               </p>
             </div>
-            <div>
+            <div className={style.innerSecDivs}>
+              <img src="background: url(trend.png)" />
+
+              <h3>24hrs Payment</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi
-                in nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                Confirmation of payment in 24 hours; once our terms and
+                conditions for fees has been agreed to.
               </p>
             </div>
           </div>
         </div>
       </section>
+      <div className={style.blurback}>
+        <div className={style.blur}>
+          <p className={style.bold}>
+            <h1>The numbers don't lie</h1>
+            Over the years , we have built an unbeatable trust between our
+            clients and partners
+          </p>
+          <p style={{ color: "black", marginLeft: "50vh" }}>
+            <h2>
+              Volume of transactions 2019(*
+              <span style={{ color: "#B0D235" }}>Millions</span>){" "}
+            </h2>
+          </p>
+          <section>
+            <div className={style.volumes}>
+              <h3>ROSABON</h3>
+              <h3>AQUILA CAPITAL</h3>
+              <h3>ACCESS BANK</h3>
+              <h3>GLO</h3>
+              <h3>ICS</h3>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
